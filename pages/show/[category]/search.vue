19e88ca7e40ec2fcc5f0d991daffd8f16551839c
XushnayevAsadbek <xushnaevasadbek@gmial.com>
2024-02-18 10:41:57 +0500

    talim  page and search page commit

---
+++ pages/show/[category]/search.vue
@@ -0,0 +1,247 @@
+<script setup>
+useHead({ title: "Bolalar olami | posts" });
+</script>
+
+<template>
+    <main>
+    <section class="container">
+       <div class="search-result">
+       <div class="search-result__box ">
+        <p class="search-result__text ">Kalit so’zi
+        </p>
+        <img src="/images/searchImages/close-icon.svg" class="img-fluid" alt="close icon">
+       </div>
+
+       <div class="search-result__card">
+        <div class="search-result__card-wrapper position-relative">
+            <img src="/images/searchImages/img1.png" class="img-fluid w-100" alt="img">
+            <div class="search-result__card-wrapper-box position-absolute w-100 h-100">
+                <p class="search-result__card-wrapper-text position-absolute position-absolute">
+                    Yaponiyada tug‘ilishlar soni keskin kamaydi
+                </p>
+            </div>
+        </div>
+        <div class="search-result__card-wrapper position-relative">
+            <img src="/images/searchImages/img1.png" class="img-fluid w-100" alt="img">
+            <div class="search-result__card-wrapper-box position-absolute w-100 h-100">
+                <p class="search-result__card-wrapper-text position-absolute">
+                    Yaponiyada tug‘ilishlar soni keskin kamaydi
+                </p>
+            </div>
+        </div>
+        <div class="search-result__card-wrapper position-relative">
+            <img src="/images/searchImages/img1.png" class="img-fluid w-100" alt="img">
+            <div class="search-result__card-wrapper-box position-absolute w-100 h-100">
+                <p class="search-result__card-wrapper-text position-absolute">
+                    Yaponiyada tug‘ilishlar soni keskin kamaydi
+                </p>
+            </div>
+        </div>
+        <div class="search-result__card-wrapper position-relative">
+            <img src="/images/searchImages/img1.png" class="img-fluid w-100" alt="img">
+            <div class="search-result__card-wrapper-box position-absolute w-100 h-100">
+                <p class="search-result__card-wrapper-text position-absolute">
+                    Yaponiyada tug‘ilishlar soni keskin kamaydi
+                </p>
+            </div>
+        </div>
+        <div class="search-result__card-wrapper position-relative">
+            <img src="/images/searchImages/img1.png" class="img-fluid w-100" alt="img">
+            <div class="search-result__card-wrapper-box position-absolute w-100 h-100">
+                <p class="search-result__card-wrapper-text position-absolute">
+                    Yaponiyada tug‘ilishlar soni keskin kamaydi 
+                </p>
+            </div>
+        </div>
+        <div class="search-result__card-wrapper position-relative">
+            <img src="/images/searchImages/img1.png" class="img-fluid w-100" alt="img">
+            <div class="search-result__card-wrapper-box position-absolute w-100 h-100">
+                <p class="search-result__card-wrapper-text position-absolute">
+                    Yaponiyada tug‘ilishlar soni keskin kamaydi
+                </p>
+            </div>
+        </div>
+        <div class="search-result__card-wrapper position-relative">
+            <img src="/images/searchImages/img1.png" class="img-fluid w-100" alt="img">
+            <div class="search-result__card-wrapper-box position-absolute w-100 h-100">
+                <p class="search-result__card-wrapper-text position-absolute">
+                    Yaponiyada tug‘ilishlar soni keskin kamaydi
+                </p>
+            </div>
+        </div>
+        <div class="search-result__card-wrapper position-relative">
+            <img src="/images/searchImages/img1.png" class="img-fluid w-100" alt="img">
+            <div class="search-result__card-wrapper-box position-absolute w-100 h-100">
+                <p class="search-result__card-wrapper-text position-absolute">
+                    Yaponiyada tug‘ilishlar soni keskin kamaydi
+                </p>
+            </div>
+        </div>
+       </div>
+       </div>
+    </section>
+    <section class="lastNews">
+        <div class="container">
+            <div class="lastNews__wrapper search-news">
+                <div class="lastNews__left">
+                    <div class="lastNews__left-inner card h-100 shadow-0 rounded-0 darkMode">
+                        <div class="position-relative">
+                            <img src="/images/lastNew1.jpg" class="card-img-top rounded-0"
+                                alt="Hollywood Sign on The Hill" />
+                            <span class="position-absolute lastNews__left-spLink darkMode">Salomatlik</span>
+                        </div>
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title darkMode">Mirziyoyeva oilasi bolalar kutubga bordi.
+                            </h5>
+                            <p class="card-text darkMode">
+                                Prezident yordamchisi Saida Mirziyoyeva farzandlari bilan bolalar
+                                kutubxonasiga keldi.....
+                            </p>
+                            <span class="lastNews__left-sp darkMode-sp">12.12.2023 12:32</span>
+                        </div>
+                    </div>
+                    <div class="lastNews__left-inner card h-100 shadow-0 rounded-0 darkMode">
+                        <div class="position-relative">
+                            <img src="/images/lastNew2.jpg" class="card-img-top rounded-0"
+                                alt="Hollywood Sign on The Hill" />
+                            <span class="position-absolute lastNews__left-spLink darkMode">Salomatlik</span>
+                        </div>
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title darkMode">Mirziyoyeva oilasi bolalar kutubga bordi.
+                            </h5>
+                            <p class="card-text darkMode">
+                                Prezident yordamchisi Saida Mirziyoyeva farzandlari bilan bolalar
+                                kutubxonasiga keldi.....
+                            </p>
+                            <span class="lastNews__left-sp darkMode-sp">12.12.2023 12:32</span>
+                        </div>
+                    </div>
+                    <div class="lastNews__left-inner card h-100 shadow-0 rounded-0 darkMode">
+                        <div class="position-relative">
+                            <img src="/images/lastNew3.jpg" class="card-img-top rounded-0"
+                                alt="Hollywood Sign on The Hill" />
+                            <span class="position-absolute lastNews__left-spLink darkMode">Salomatlik</span>
+                        </div>
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title darkMode">Mirziyoyeva oilasi bolalar kutubga bordi.
+                            </h5>
+                            <p class="card-text darkMode">
+                                Prezident yordamchisi Saida Mirziyoyeva farzandlari bilan bolalar
+                                kutubxonasiga keldi.....
+                            </p>
+                            <span class="lastNews__left-sp darkMode-sp">12.12.2023 12:32</span>
+                        </div>
+                    </div>
+
+
+                    <div class="lastNews__left-inner card h-100 shadow-0 rounded-0 darkMode">
+                        <div class="position-relative">
+                            <img src="/images/lastNew4.jpg" class="card-img-top rounded-0"
+                                alt="Hollywood Sign on The Hill" />
+                            <span class="position-absolute lastNews__left-spLink darkMode">Salomatlik</span>
+                        </div>
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title darkMode">Mirziyoyeva oilasi bolalar kutubga bordi.
+                            </h5>
+                            <p class="card-text darkMode">
+                                Prezident yordamchisi Saida Mirziyoyeva farzandlari bilan bolalar
+                                kutubxonasiga keldi.....
+                            </p>
+                            <span class="lastNews__left-sp darkMode-sp">12.12.2023 12:32</span>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="lastNews__right darkMode border-0">
+                    <h4 class="lastNews__right-title ">So'nggi yangiliklarga</h4>
+                    <ul class="lastNews__right-list list-unstyled darkMode">
+                        <li class="border-bottom">
+                            <p class="lastNews__right-text darkMode">Ertaga O‘zbekistonning qaysi hududlarida
+                                qor
+                                yog‘ishi ma’lum qilindi</p>
+                            <div
+                                class=" lastNews__right-view d-flex justify-content-between align-items-center  darkMode-sp">
+                                <div class="d-flex align-items-center" style="gap: 7px;">
+                                    <img src="/images/eye.svg" alt="eye icon">
+                                    <span class="lastNews__right-sp darkMode-sp">80934</span>
+                                </div>
+                                <span
+                                    class="lastNews__right-sp lastNews__right-spDate darkMode-sp">12.12.2023</span>
+                            </div>
+                        </li>
+                        <li class="border-bottom">
+                            <p class="lastNews__right-text darkMode">Ertaga O‘zbekistonning qaysi hududlarida qor
+                                yog‘ishi ma’lum qilindi</p>
+                            <div
+                                class="lastNews__right-view d-flex justify-content-between align-items-center darkMode-sp">
+                                <div class="d-flex align-items-center" style="gap: 7px;">
+                                    <img src="/images/eye.svg" alt="eye icon">
+                                    <span class="lastNews__right-sp darkMode-sp">80934</span>
+                                </div>
+                                <span
+                                    class="lastNews__right-sp lastNews__right-spDate darkMode-sp">12.12.2023</span>
+                            </div>
+                        </li>
+                        <li class="border-bottom">
+                            <p class="lastNews__right-text darkMode">Ertaga O‘zbekistonning qaysi hududlarida
+                                qor
+                                yog‘ishi ma’lum qilindi</p>
+                            <div
+                                class=" lastNews__right-view d-flex justify-content-between align-items-center  darkMode-sp">
+                                <div class="d-flex align-items-center" style="gap: 7px;">
+                                    <img src="/images/eye.svg" alt="eye icon">
+                                    <span class="lastNews__right-sp darkMode-sp">80934</span>
+                                </div>
+                                <span
+                                    class="lastNews__right-sp lastNews__right-spDate darkMode-sp">12.12.2023</span>
+                            </div>
+                        </li>
+                        <li class="border-bottom">
+                            <p class="lastNews__right-text darkMode">Ertaga O‘zbekistonning qaysi hududlarida
+                                qor
+                                yog‘ishi ma’lum qilindi</p>
+                            <div
+                                class=" lastNews__right-view d-flex justify-content-between align-items-center  darkMode-sp">
+                                <div class="d-flex align-items-center" style="gap: 7px;">
+                                    <img src="/images/eye.svg" alt="eye icon">
+                                    <span class="lastNews__right-sp darkMode-sp">80934</span>
+                                </div>
+                                <span
+                                    class="lastNews__right-sp lastNews__right-spDate darkMode-sp">12.12.2023</span>
+                            </div>
+                        </li>
+                        <li class="border-bottom">
+                            <p class="lastNews__right-text darkMode">Ertaga O‘zbekistonning qaysi hududlarida
+                                qor
+                                yog‘ishi ma’lum qilindi</p>
+                            <div
+                                class=" lastNews__right-view d-flex justify-content-between align-items-center  darkMode-sp">
+                                <div class="d-flex align-items-center" style="gap: 7px;">
+                                    <img src="/images/eye.svg" alt="eye icon">
+                                    <span class="lastNews__right-sp darkMode-sp">80934</span>
+                                </div>
+                                <span
+                                    class="lastNews__right-sp lastNews__right-spDate darkMode-sp">12.12.2023</span>
+                            </div>
+                        </li>
+                        <li class="border-bottom">
+                            <p class="lastNews__right-text darkMode">Ertaga O‘zbekistonning qaysi hududlarida
+                                qor
+                                yog‘ishi ma’lum qilindi</p>
+                            <div
+                                class=" lastNews__right-view d-flex justify-content-between align-items-center  darkMode-sp">
+                                <div class="d-flex align-items-center" style="gap: 7px;">
+                                    <img src="/images/eye.svg" alt="eye icon">
+                                    <span class="lastNews__right-sp darkMode-sp">80934</span>
+                                </div>
+                                <span
+                                    class="lastNews__right-sp lastNews__right-spDate darkMode-sp">12.12.2023</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+ </main>
+</template>
