1a89d3fbd81ef42bcd187ebe607224ce72e4905d
Turdaliyev Abdurahmon <121414053+Abdurahmon086@users.noreply.github.com>
2024-02-26 22:04:30 +0500

    brend slider updated

--- components/loopScroler/loopScroler.vue
+++ components/loopScroler/loopScroler.vue
@@ -1,181 +1,31 @@
 <template>
     <section
-        class="slide-option loopScroler bg-white darkMode mt-5 mb-0 d-none d-md-flex"
+        class="loopScroler bg-white darkMode mt-5 mb-0 d-none d-md-flex w-100"
     >
-        <div id="infinite" class="highway-slider">
-            <div class="highway-barrier">
-                <ul class="highway-lane list-unstyled m-0 darkMode">
-                    <li class="highway-car">
-                        <img
-                            src="/images/dunyoBola-img.svg"
-                            alt="dunyoBola-img"
-                            class="lightIcon d-block img-fluid"
-                        />
-                        <img
-                            src="/images/dunyoBola-img-dark1.jpg"
-                            alt="dunyoBola-img"
-                            class="darkIcon d-none darkMode img-fluid"
-                            style="width: 204px"
-                        />
-                    </li>
-                    <li class="highway-car">
-                        <img
-                            src="/images/dunyoBola-img.svg"
-                            alt="dunyoBola-img"
-                            class="lightIcon d-block img-fluid"
-                        />
-                        <img
-                            src="/images/dunyoBola-img-dark1.jpg"
-                            alt="dunyoBola-img"
-                            class="darkIcon d-none darkMode img-fluid"
-                            style="width: 204px"
-                        />
-                    </li>
-                    <li class="highway-car">
-                        <img
-                            src="/images/dunyoBola-img.svg"
-                            alt="dunyoBola-img"
-                            class="lightIcon d-block img-fluid"
-                        />
-                        <img
-                            src="/images/dunyoBola-img-dark1.jpg"
-                            alt="dunyoBola-img"
-                            class="darkIcon d-none darkMode img-fluid"
-                            style="width: 204px"
-                        />
-                    </li>
-                    <li class="highway-car">
-                        <img
-                            src="/images/dunyoBola-img.svg"
-                            alt="dunyoBola-img"
-                            class="lightIcon d-block img-fluid"
-                        />
-                        <img
-                            src="/images/dunyoBola-img-dark1.jpg"
-                            alt="dunyoBola-img"
-                            class="darkIcon d-none darkMode img-fluid"
-                            style="width: 204px"
-                        />
-                    </li>
-                    <li class="highway-car">
-                        <img
-                            src="/images/dunyoBola-img.svg"
-                            alt="dunyoBola-img"
-                            class="lightIcon d-block img-fluid"
-                        />
-                        <img
-                            src="/images/dunyoBola-img-dark1.jpg"
-                            alt="dunyoBola-img"
-                            class="darkIcon d-none darkMode img-fluid"
-                            style="width: 204px"
-                        />
-                    </li>
-                    <li class="highway-car">
-                        <img
-                            src="/images/dunyoBola-img.svg"
-                            alt="dunyoBola-img"
-                            class="lightIcon d-block img-fluid"
-                        />
-                        <img
-                            src="/images/dunyoBola-img-dark1.jpg"
-                            alt="dunyoBola-img"
-                            class="darkIcon d-none darkMode img-fluid"
-                            style="width: 204px"
-                        />
-                    </li>
-                    <li class="highway-car">
-                        <img
-                            src="/images/dunyoBola-img.svg"
-                            alt="dunyoBola-img"
-                            class="lightIcon d-block img-fluid"
-                        />
-                        <img
-                            src="/images/dunyoBola-img-dark1.jpg"
-                            alt="dunyoBola-img"
-                            class="darkIcon d-none darkMode img-fluid"
-                            style="width: 204px"
-                        />
-                    </li>
-                    <li class="highway-car">
-                        <img
-                            src="/images/dunyoBola-img.svg"
-                            alt="dunyoBola-img"
-                            class="lightIcon d-block img-fluid"
-                        />
-                        <img
-                            src="/images/dunyoBola-img-dark1.jpg"
-                            alt="dunyoBola-img"
-                            class="darkIcon d-none darkMode img-fluid"
-                            style="width: 204px"
-                        />
-                    </li>
-                    <li class="highway-car">
-                        <img
-                            src="/images/dunyoBola-img.svg"
-                            alt="dunyoBola-img"
-                            class="lightIcon d-block img-fluid"
-                        />
-                        <img
-                            src="/images/dunyoBola-img-dark1.jpg"
-                            alt="dunyoBola-img"
-                            class="darkIcon d-none darkMode img-fluid"
-                            style="width: 204px"
-                        />
-                    </li>
-                    <li class="highway-car">
-                        <img
-                            src="/images/dunyoBola-img.svg"
-                            alt="dunyoBola-img"
-                            class="lightIcon d-block img-fluid"
-                        />
-                        <img
-                            src="/images/dunyoBola-img-dark1.jpg"
-                            alt="dunyoBola-img"
-                            class="darkIcon d-none darkMode img-fluid"
-                            style="width: 204px"
-                        />
-                    </li>
-                    <li class="highway-car">
-                        <img
-                            src="/images/dunyoBola-img.svg"
-                            alt="dunyoBola-img"
-                            class="lightIcon d-block img-fluid"
-                        />
-                        <img
-                            src="/images/dunyoBola-img-dark1.jpg"
-                            alt="dunyoBola-img"
-                            class="darkIcon d-none darkMode img-fluid"
-                            style="width: 204px"
-                        />
-                    </li>
-                    <li class="highway-car">
-                        <img
-                            src="/images/dunyoBola-img.svg"
-                            alt="dunyoBola-img"
-                            class="lightIcon d-block img-fluid"
-                        />
-                        <img
-                            src="/images/dunyoBola-img-dark1.jpg"
-                            alt="dunyoBola-img"
-                            class="darkIcon d-none darkMode img-fluid"
-                            style="width: 204px"
-                        />
-                    </li>
-                    <li class="highway-car">
-                        <img
-                            src="/images/dunyoBola-img.svg"
-                            alt="dunyoBola-img"
-                            class="lightIcon d-block img-fluid"
-                        />
-                        <img
-                            src="/images/dunyoBola-img-dark1.jpg"
-                            alt="dunyoBola-img"
-                            class="darkIcon d-none darkMode img-fluid"
-                            style="width: 204px"
-                        />
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <Swiper
+            :modules="[SwiperEffectCreative, SwiperNavigation, SwiperFreeMode]"
+            :slides-per-view="5"
+            :navigation="{ enabled: true }"
+            class="loopScroler__inner"
+        >
+            <SwiperSlide
+                v-for="slide in 100"
+                :key="slide"
+                class="loopScroler__wrapper list-unstyled m-0 darkMode"
+            >
+                <li class="loopScroler__wrapper-item d-flex">
+                    <img
+                        src="/images/dunyoBola-img.svg"
+                        alt="dunyoBola-img"
+                        class="lightIcon d-block img-fluid"
+                    />
+                    <img
+                        src="/images/dunyoBola-img-dark1.jpg"
+                        alt="dunyoBola-img"
+                        class="darkIcon d-none darkMode img-fluid"
+                    />
+                </li>
+            </SwiperSlide>
+        </Swiper>
     </section>
 </template>
