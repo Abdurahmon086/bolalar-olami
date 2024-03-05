b554a273d8ada2b8b13ace2b75b64190324ef5e6
Turdaliyev Abdurahmon <121414053+Abdurahmon086@users.noreply.github.com>
2024-03-05 21:01:16 +0500

    ok

--- components/loopScroler/loopScroler.vue
+++ components/loopScroler/loopScroler.vue
@@ -4,8 +4,7 @@
             <SwiperSlide v-for="slide in 100" :key="slide" class="loopScroler__wrapper list-unstyled m-0 darkMode">
                 <li class="loopScroler__wrapper-item d-flex">
                     <img src="/images/dunyoBola-img.svg" alt="dunyoBola-img" class="lightIcon d-block img-fluid" />
-                    <img src="/images/dunyoBola-img-dark1.jpg" alt="dunyoBola-img"
-                        class="darkIcon d-none darkMode img-fluid" />
+                    <img src="/images/dunyoBola-img-dark1.jpg" alt="dunyoBola-img" class="darkIcon d-none img-fluid" />
                 </li>
             </SwiperSlide>
         </Swiper>
